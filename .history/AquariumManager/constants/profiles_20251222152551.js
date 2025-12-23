37064f439bf651f5911fea4acf595f5cdda347c6
ChristopherCardoza <cardozachristopher1@gmail.com>
2025-12-22 20:47:11 -0500

    incomplete profiles.js added

---
+++ .history/AquariumManager/constants/profiles_20251222152551.js
@@ -0,0 +1,18 @@
+const user1 = {
+    id: 'user-1',
+    username: 'AqauriumLover123',
+    profilePic: '', // add pic url here later
+    tanks: [
+        {
+            id: 'tank-1-1',
+            name: 'My lovely aquarium',
+            size: 20,
+            temperature: 80,
+            ph: 7,
+            gh: 6,
+            image: '', // put the same tank pic here as what i had for figma
+            // add fish
+        }
+       
+    ]
+};
