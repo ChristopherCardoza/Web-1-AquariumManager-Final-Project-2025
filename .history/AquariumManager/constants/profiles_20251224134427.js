e43d97db8d6b82fa583ebb54ae33b4db14952a87
ChristopherCardoza <cardozachristopher1@gmail.com>
2025-12-24 13:47:14 -0500

    database schema linked to readme

---
+++ .history/AquariumManager/constants/profiles_20251224134427.js
@@ -0,0 +1,74 @@
+const user1 = {
+  id: "user-1",
+  username: "AqauriumLover123",
+  profilePic: "../../public/profile-user1.jpg",
+  tanks: [
+    {
+      id: "tank-1-1",
+      name: "My lovely aquarium",
+      size: 20,
+      temperature: 80,
+      ph: 7,
+      gh: 6,
+      image: "../../public/tank-1-1.png",
+      fish: [
+        {
+          fishId: 1,
+          id: 1,
+          name: "betta",
+          quantity: 1,
+        },
+        {
+          fishId: 2,
+          id: 2,
+          name: "guppy",
+          quantity: 3,
+        },
+        {
+          fishId: 3,
+          id: 3,
+          name: "neon tetra",
+          quantity: 6,
+        },
+      ],
+    },
+  ],
+};
+
+const user2 = {
+  id: "user-2",
+  username: "GuppyKeeper11",
+  profilePic: "../../public/profile-user2.jpg",
+  tanks: [
+    {
+      id: "tank-2-1",
+      name: "guppy fry tank",
+      size: 10,
+      temperature: 80,
+      ph: 7.4,
+      gh: 6,
+      image: "../../public/tank-2-1.jpg",
+      fish: [
+        {
+          fishId: 2,
+          id: 2,
+          name: "guppy",
+          quantity: 22,
+        },
+      ],
+    },
+  ],
+};
+
+export { user1, user2 };
+
+export function ProfileById(id) {
+  if (id == "user-1") {
+    return user1;
+  }
+  if (id == "user-2") {
+    return user2;
+  }
+
+  return null;
+}
